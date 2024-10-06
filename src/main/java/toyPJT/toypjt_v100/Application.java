c743f841b7276740e232af38f46050f0b48bb7b9
kth <rlaxogns9024@naver.com>
2024-10-06 22:14:02 +0900

    first commit

---
+++ src/main/java/toyPJT/toypjt_v100/Application.java
@@ -0,0 +1,13 @@
+package toyPJT.toypjt_v100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
